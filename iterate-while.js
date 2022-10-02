434216d07d913e0943cce4966f0cfa800f10d21e
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2022-10-02 13:25:11 -0400

    First attempt at trying a while loop.

---
+++ iterate-while.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name        Expand journal article text.
+// @description Expand journal article text on APS journals.
+// @grant       none
+// @match       http://journals.aps.org/*
+// @match       https://journals.aps.org/*
+// @author      Nathaniel Morck Foley-Beaver
+// @namespace   https://github.com/nbeaver/
+// @license     MIT
+// @namespace   https://github.com/nbeaver/
+// @homepageURL  https://github.com/nbeaver/userscript-expand-article-text
+// @version     0.1
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  var N_current = 0;
+  var N_prev = -1; // always run first time
+  while (N_current > N_prev) {
+    var expandable_elems = document.getElementsByClassName('expandable');
+    var i;
+    for (i = 0; i < expandable_elems.length; i++) {
+      var elem = expandable_elems[i];
+      if (elem.lastChild.classList.contains('expandable-body')) {
+        // Already expanded, do nothing.
+      } else {
+        var first_child = elem.children[0];
+        first_child.click();
+      }
+    }
+    N_prev = expandable_elems.length;
+    N_current = document.getElementsByClassName('expandable').length;
+  }  
+})();
